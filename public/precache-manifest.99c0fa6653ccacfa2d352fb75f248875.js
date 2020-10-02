25938ed38ce44dff85c4ecfe1198247eb9650c42
Jamal Pelpuo <jpelpuo@ymail.com>
2020-10-02 13:17:46 +0000

    Adds front end

--- public/precache-manifest.99c0fa6653ccacfa2d352fb75f248875.js
+++ public/precache-manifest.99c0fa6653ccacfa2d352fb75f248875.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "efd1659af98791910c86c4255f0c2a37",
+    "revision": "e397709da9b0eb7ae1535d7413890f9c",
     "url": "/index.html"
   },
   {
-    "revision": "c1849fd39dc39251c1ff",
+    "revision": "a67ebb2fa965be7864a9",
     "url": "/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "4fa6eddaa7640d9bc44b",
+    "revision": "d219e576527f6cad8792",
     "url": "/static/css/main.8838ba68.chunk.css"
   },
   {
-    "revision": "c1849fd39dc39251c1ff",
-    "url": "/static/js/2.e9c20c50.chunk.js"
+    "revision": "a67ebb2fa965be7864a9",
+    "url": "/static/js/2.a4cbca35.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.e9c20c50.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a4cbca35.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4fa6eddaa7640d9bc44b",
-    "url": "/static/js/main.ae8bd0b5.chunk.js"
+    "revision": "d219e576527f6cad8792",
+    "url": "/static/js/main.16cde36a.chunk.js"
   },
   {
     "revision": "969ec6062bb99b0b9815",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eeb2af45412f1f23e85a1724b93210e3.js"
+  "/precache-manifest.99c0fa6653ccacfa2d352fb75f248875.js"
 );
 
 self.addEventListener('message', (event) => {
