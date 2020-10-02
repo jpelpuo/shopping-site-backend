26813153c612dde67f1b3bbc4eb46ccd6326fdb3
Jamal Pelpuo <jpelpuo@ymail.com>
2020-10-02 03:50:21 +0000

    Adds front end

---
+++ public/precache-manifest.0a7fe1044f256a5c1c7274fe7d3d5bca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75178505fbcd61745f39450e1373d06a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5fa187445401c3af72",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b85e63476db18a64f72c",
+    "url": "/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "5c5fa187445401c3af72",
+    "url": "/static/js/2.6c4598f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6c4598f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b85e63476db18a64f72c",
+    "url": "/static/js/main.b604c464.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  }
+]);
