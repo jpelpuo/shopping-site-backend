93e9f4136ed210ac9412d326392290dabfc33e62
Jamal Pelpuo <jpelpuo@ymail.com>
2020-10-02 11:13:44 +0000

    Integration with backend

---
+++ public/build/precache-manifest.013358d86d2d48a4aa30f9a4ad0f3dd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf0011f622c716a55a9562a212ea2e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ff933f8e0f51bc9f7f",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a9c4919b096c41c3dcd6",
+    "url": "/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "89ff933f8e0f51bc9f7f",
+    "url": "/static/js/2.ebe4d7d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ebe4d7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c4919b096c41c3dcd6",
+    "url": "/static/js/main.dc967c89.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  }
+]);
